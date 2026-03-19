d9d6b948182177cbce85fb186f04fc5e158b9309
Hugo Garcia <garcia.hmg10@gmail.com>
2026-03-19 17:20:52 -0300

    Add Italian Grand Prix and race data to DML.sql

--- DML.sql
+++ DML.sql
@@ -34,3 +34,10 @@ INSERT INTO corridas (nome) VALUES
 ("Grande Prêmio da Inglaterra"),
 ("Grande Prêmio da Bélgica"),
 ("Grande Prêmio da Itália");
+
+INSERT INTO dados_corridas (id_corrida, id_equipe, tempo_volta) VALUES 
+(1, 1, '00:01:14.500'), 
+(1, 3, '00:01:13.200'), 
+(2, 2, '00:01:16.400'), 
+(3, 4, '00:01:28.100'), 
+(5, 5, '00:01:32.700'); 
