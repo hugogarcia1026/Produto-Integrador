2077de7d4d2a0d159fd0513a05d592d7c21fee9e
Hugo Garcia <garcia.hmg10@gmail.com>
2026-03-19 17:20:24 -0300

    Create melhor_volta function to find best lap time
    Added function to calculate the best lap time for a team in a race.

--- function_pi_idev4.sql
+++ function_pi_idev4.sql
@@ -45,3 +45,30 @@ SET media = (
 // DELIMITER ;
 
 SELECT tempo_medio_volta(1, 1);
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+DROP FUNCTION IF EXISTS melhor_volta;
+DELIMITER //
+
+CREATE FUNCTION melhor_volta(
+    _id_equipe INT,
+    _id_corrida INT
+)
+RETURNS TIME
+READS SQL DATA 
+BEGIN
+    DECLARE v_volta TIME;
+
+    SELECT MIN(tempo_volta) INTO v_volta
+    FROM dados_corridas  
+    WHERE id_corrida = _id_corrida
+    AND id_equipe = _id_equipe;
+    
+    RETURN v_volta;
+END
+// 
+DELIMITER ;
+
+SELECT melhor_volta(1, 1);
+
